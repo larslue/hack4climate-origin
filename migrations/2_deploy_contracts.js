0ef7d4b319b48b78b15efebe456101ae9764541c
Florian Liss <flo.liss@tum.de>
2017-11-15 00:40:58 +0100

    Compiled Contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,19 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
+var Factory = artifacts.require("./factory.sol");
+var Origins = artifacts.require("./Origins.sol");
+var OriginalToken = artifacts.require("./OriginalCoin.sol");
+var Storage = artifacts.require("./storage.sol");
+
+
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+  deployer.deploy(Factory);
+  deployer.deploy(Origins);
+  deployer.deploy(OriginalToken);
+  deployer.deploy(Storage);
+  deployer.autolink();
 };

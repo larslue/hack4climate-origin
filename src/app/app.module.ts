3a635201211ceb8bc232aeab2578e0716ffabe00
Lukas Mohs <ga47boq@mytum.de>
2017-11-14 23:47:48 +0100

    fixed form problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+
 
 // App
 import { AppComponent } from './app.component';
@@ -14,11 +15,11 @@ import { MetaModule } from './meta/meta.module';
     AppComponent
   ],
   imports: [
-    BrowserModule,
     FormsModule,
     HttpModule,
     MaterialModule,
     MetaModule,
+      BrowserModule,
     BrowserAnimationsModule
   ],
   providers: [],

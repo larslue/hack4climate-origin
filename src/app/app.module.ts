be9f2919f8bf01291e5e7c51585f4e8efbcb2366
Lukas Mohs <ga47boq@mytum.de>
2017-11-14 22:56:34 +0100

    changezz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // Material UI
 import {
   MatButtonModule,
-  MatCheckboxModule
+  MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule
 } from '@angular/material';
 
 import {
   MatTabsModule
 } from '@angular/material/tabs';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // App
 import { AppComponent } from './app.component';
@@ -28,6 +31,8 @@ import { MetaModule } from './meta/meta.module';
     FormsModule,
     HttpModule,
     // Material UI
+    MatFormFieldModule,
+    MatInputModule,
     MatButtonModule,
     MatCheckboxModule,
     MatTabsModule,

6eb21c5493cec5c790c9eec6b222b220ea918de5
Lukas Mohs <ga47boq@mytum.de>
2017-11-15 09:35:27 +0100

    removed old challenger code reintegrated from lars :D

--- src/app/meta/challenger/challenger.component.ts
+++ src/app/meta/challenger/challenger.component.ts
@@ -1,7 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {Web3Service} from '../../util/web3.service';
-import factory_artifacts from '../../../../build/contracts/OriginFactory.json';
-import origin_artifacts from '../../../../build/contracts/Origin.json';
 
 @Component({
   selector: 'app-challenger',
@@ -9,12 +7,6 @@ import origin_artifacts from '../../../../build/contracts/Origin.json';
   styleUrls: ['./challenger.component.css']
 })
 export class ChallengerComponent implements OnInit {
-  factoryAbstraction: any;
-  originAbstraction: any;
-
-  activeOrigins = [];
-  activeOriginContracts = [];
-
 
   model = {
     name: 'blabla'
@@ -27,38 +19,7 @@ export class ChallengerComponent implements OnInit {
   ngOnInit(): void {
     console.log('Challenger OnInit: ' + this.web3Service);
     console.log(this);
-    this.web3Service.artifactsToContract(factory_artifacts)
-      .then((factoryAbstraction) => {
-        this.factoryAbstraction = factoryAbstraction;
-        this.refreshBullshits();
-      });
-    this.web3Service.artifactsToContract(origin_artifacts)
-      .then((originAbstraction) => {
-        this.originAbstraction = originAbstraction;
-      });
 
-  }
-
-  async refreshBullshits() {
-    console.log('Refreshing bullshits');
-
-    try {
-      const deployedFactory = await this.factoryAbstraction.deployed();
-      console.log(deployedFactory, "deployedFactory");
-      // FIXME: this does many things but working is not one of them.
-      const maxElements = 10;
-      for (let i = 0; i < maxElements; i++) {
-        const activeOrigin = await deployedFactory.getActiveOrigins.call({index: i});
-        console.log("this origin", activeOrigin);
-        if (activeOrigin) {
-          this.activeOrigins.push(activeOrigin);
-        }
-      }
-      console.log('Found origins: ' + this.activeOrigins);
-    } catch (e) {
-      console.log(e);
-      console.log('Error getting balance; see log.');
-    }
   }
 
 
